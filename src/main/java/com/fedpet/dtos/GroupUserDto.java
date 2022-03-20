1963abdbcbe841c4177db883ad0e91814e0361c0
Priyanshu1990 <priyanshuparate10@gmail.com>
2022-03-20 21:51:38 +0530

    fix issues

--- src/main/java/com/fedpet/dtos/GroupUserDto.java
+++ src/main/java/com/fedpet/dtos/GroupUserDto.java
@@ -14,7 +14,7 @@ public class GroupUserDto {
     public static GroupUserDto build(UserGroup userGroup) {
       var groupUserDto=  new GroupUserDto();
         groupUserDto.setId(userGroup.getUser().getId());
-        groupUserDto.setLastname(userGroup.getUser().getUsername());
+        groupUserDto.setFirstname(userGroup.getUser().getFirstname());
         groupUserDto.setLastname(userGroup.getUser().getLastname());
         groupUserDto.setPrimaryUserType(userGroup.getUserType());
         return groupUserDto;
